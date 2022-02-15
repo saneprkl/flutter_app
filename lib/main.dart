205123150567c49488f48413f341777707eabaae
saneprkl <santeri.lehtonen@gmail.com>
2022-02-15 12:33:52 +0200

    buttons

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    
+    print('Answer chosen!');
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar( title: Text('First App'),),
+      body: Column(children: [ Text(questions[questionIndex],
+      ),
+      RaisedButton(child: Text('Answer 1'),
+      onPressed: answerQuestion,),
+      RaisedButton(child: Text('Answer 2'),
+      onPressed: answerQuestion,),
+      RaisedButton(child: Text('Answer 3'),
+      onPressed: answerQuestion,),],),
+      ),
+    );
+  }
+}
